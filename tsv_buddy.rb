55309b55ca98a17781746a44c50af2e06a017175
KOUTOU Odilon <odilonkoutou@yahoo.fr>
2017-09-29 09:33:10 +0800

    Solved the homework and all tests pass

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,36 @@
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
+  TAB = "\t".freeze
+  NEWLINE = "\n".freeze
+
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
+    @data = []
+    tsv_source = tsv.split(NEWLINE)
+    header = tsv_source[0].split(TAB)
+    tsv_source.each_with_index do |line, index|
+      next if index.zero?
+      @data.push(hash_tsv_line(line, header))
+    end
+    @data
+  end
 
+  def hash_tsv_line(line, header)
+    row = {}
+    line.split(TAB).each_with_index { |col, id| row[header[id]] = col }
+    row
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
+    output = to_tsv_header << NEWLINE
+    @data.map { |line| output << line.values.join(TAB) << NEWLINE }
+    output
+  end
 
+  def to_tsv_header
+    @data[0].keys.join(TAB)
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,25 @@
 # Module that can be included (mixin) to take and output Yaml data
+module YamlBuddy
+  # take_yaml: converts a String with YAML data into @data
+  # parameter: yaml - a String in YAML format
+  def take_yaml(yaml)
+    @data = []
+    yaml_source = YAML.safe_load(yaml)
+    yaml_source.each do |line|
+      @data.push(hash_yaml_line(line))
+    end
+    @data
+  end
+
+  def hash_yaml_line(line)
+    row = {}
+    line.each_with_index { |(key, value), _| row[key] = value }
+    row
+  end
+
+  # to_yaml: converts @data into yaml string
+  # returns: String in YAML format
+  def to_yaml
+    @data.to_yaml
+  end
+end
